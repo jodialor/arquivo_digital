0f9bbbdf81313d42c666b3cdb26070675f6b7385
jodialor <diogornelas1@gmail.com>
2018-02-08 16:53:50 +0000

    header navbar colocada num ficheiro separado

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,24 +1,51 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <span class="logo-mini"><font size="6"><p><i class="fa fa-archive" aria-hidden="true"></i> Arquivo Digital de Documentação &nbsp;&nbsp;</p></font></span>
+      <a class="navbar-brand" href="#">
+        <span class="logo-mini"><font size="6"><p><i class="fa fa-archive" aria-hidden="true"></i> Arquivo Digital de Documentação &nbsp;&nbsp;</p></font></span>
+      </a>
     </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-      
-          <li><a href="{{route('register')}}">Registar</a></li>
-          <li><a href="{{route('login')}}">Login</a></li>
+              <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                  <!-- Left Side Of Navbar -->
+                  <ul class="nav navbar-nav">
+                      &nbsp;
+                  </ul>
 
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+                  <!-- Right Side Of Navbar -->
+                  <ul class="nav navbar-nav navbar-right">
+                      <!-- Authentication Links -->
+                      @if(Auth::guard('admin')->check())
+                        <li><a href="{{ route('admin_menu') }}">Admin Menu</a></li>
+                      @endif
+                      @if(Auth::guest())
+                          <li class="{{ Request::is('login') ? "active" : "" }}"><a href="{{ route('login') }}">Login</a></li>
+                          <li class="{{ Request::is('register') ? "active" : "" }}"><a href="{{ route('register') }}">Registar</a></li>
+                          @else
+                                <li>
+                                   <a href="{{route('dashboard')}}" class="dropdown-toggle"  role="button" aria-expanded="false">
+                                     Documentos
+                                   </a>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
+
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li>
+                                            <a href="{{route('logout')}}">Logout</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                          @endif
+                      </a>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,51 +33,7 @@
     <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
 </head>
 <body>
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="#">
-          <span class="logo-mini"><font size="6"><p><i class="fa fa-archive" aria-hidden="true"></i> Arquivo Digital de Documentação &nbsp;&nbsp;</p></font></span>
-        </a>
-      </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if(Auth::guard('admin')->check())
-                          <li><a href="{{ route('admin_menu') }}">Admin Menu</a></li>
-                        @endif
-                        @if(Auth::guest())
-                            <li class="{{ Request::is('login') ? "active" : "" }}"><a href="{{ route('login') }}">Login</a></li>
-                            <li class="{{ Request::is('register') ? "active" : "" }}"><a href="{{ route('register') }}">Register</a></li>
-                            @else
-                                  <li>
-                                     <a href="{{route('dashboard')}}" class="dropdown-toggle"  role="button" aria-expanded="false">
-                                       Documentos
-                                     </a>
-                                  </li>
-                                  <li class="dropdown">
-                                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                          {{ Auth::user()->name }} <span class="caret"></span>
-                                      </a>
-
-                                      <ul class="dropdown-menu" role="menu">
-                                          <li>
-                                              <a href="{{route('logout')}}">Logout</a>
-                                          </li>
-                                      </ul>
-                                  </li>
-                            @endif
-                        </a>
-         </div>
-       </div>
-     </nav>
+        @include('includes/header')
         @yield('content')
     </div>
 
