42ea281355abd721b5aebbca67fc9dacd7d84322
jodialor <diogornelas1@gmail.com>
2018-02-09 11:33:44 +0000

    menu de admin atualizado(retirado submenu 'admin')

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,13 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
-                <div class="panel-heading">Admin</div>
+                <div class="panel-heading">Admin Menu</div>
                 <div class="panel-body">
                     <a class="btn btn-default" href="{{route('user_table')}}">Utilizadores <i class="fa fa-users" aria-hidden="true"></i></a>
-                    <a class="btn btn-default" href="{{route('dashboard')}}">Admin <i class="fa fa-user" aria-hidden="true"></i></a>
-                    <a class="btn btn-default" href="{{route('type.doc.table')}}">Tipos de Documentos<i class="fa fa-file" aria-hidden="true"></i></a>
+                    <a class="btn btn-default" href="{{route('type.doc.table')}}">Tipos de Documentos <i class="fa fa-file" aria-hidden="true"></i></a>
                     <a class="btn btn-default" href="{{route('user.depart')}}">Departamentos <i class="fa fa-building" aria-hidden="true"></i></a>
                 </div>
             </div>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -12,40 +12,40 @@
       </a>
     </div>
 
-              <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                  <!-- Left Side Of Navbar -->
-                  <ul class="nav navbar-nav">
-                      &nbsp;
-                  </ul>
+    <div class="collapse navbar-collapse" id="app-navbar-collapse">
+      <!-- Left Side Of Navbar -->
+      <ul class="nav navbar-nav">
+          &nbsp;
+      </ul>
 
-                  <!-- Right Side Of Navbar -->
-                  <ul class="nav navbar-nav navbar-right">
-                      <!-- Authentication Links -->
-                      @if(Auth::guard('admin')->check())
-                        <li><a href="{{ route('admin_menu') }}">Admin Menu</a></li>
-                      @endif
-                      @if(Auth::guest())
-                          <li class="{{ Request::is('login') ? "active" : "" }}"><a href="{{ route('login') }}">Login</a></li>
-                          <li class="{{ Request::is('register') ? "active" : "" }}"><a href="{{ route('register') }}">Registar</a></li>
-                          @else
-                                <li>
-                                   <a href="{{route('dashboard')}}" class="dropdown-toggle"  role="button" aria-expanded="false">
-                                     Documentos
-                                   </a>
-                                </li>
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                        {{ Auth::user()->name }} <span class="caret"></span>
-                                    </a>
+      <!-- Right Side Of Navbar -->
+      <ul class="nav navbar-nav navbar-right">
+          <!-- Authentication Links -->
+          @if(Auth::guard('admin')->check())
+            <li><a href="{{ route('admin_menu') }}">Admin Menu</a></li>
+          @endif
+          @if(Auth::guest())
+              <li class="{{ Request::is('login') ? "active" : "" }}"><a href="{{ route('login') }}">Login</a></li>
+              <li class="{{ Request::is('register') ? "active" : "" }}"><a href="{{ route('register') }}">Registar</a></li>
+          @else
+              <li>
+                 <a href="{{route('dashboard')}}" class="dropdown-toggle"  role="button" aria-expanded="false">
+                   Documentos
+                 </a>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
 
-                                    <ul class="dropdown-menu" role="menu">
-                                        <li>
-                                            <a href="{{route('logout')}}">Logout</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                          @endif
-                      </a>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
+                <ul class="dropdown-menu" role="menu">
+                    <li>
+                        <a href="{{route('logout')}}">Logout</a>
+                    </li>
+                </ul>
+              </li>
+          @endif
+          </a>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
